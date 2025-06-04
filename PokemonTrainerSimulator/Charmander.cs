c1a1eb0abd2f5954c5df0c826d31290b685d2b6d
wpplng <lina.wappling@gmail.com>
2025-06-04 19:32:59 +0200

    Added comment

--- PokemonTrainerSimulator/Charmander.cs
+++ PokemonTrainerSimulator/Charmander.cs
@@ -12,9 +12,9 @@ namespace PokemonTrainerSimulator
         // Constructor for Charmander that sets the Type to Fire and initializes the base class
         public Charmander(int level, List<Attack> attacks) : base("Charmander", level, attacks) { }
 
+        // Implementation of the Evolve method from the IEvolvable interface
         public void Evolve()
         {
-            
             Name = "Charmeleon";
             Level += 10;
             Console.WriteLine($"Charmander is evolving... Now it's {Name}! Level {Level}");
