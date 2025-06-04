545eaf98cf43096181bd2ec21490f59ace363efd
wpplng <lina.wappling@gmail.com>
2025-06-04 11:34:30 +0200

    Created attack class and attacks

---
+++ PokemonTrainerSimulator/Attack.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonTrainerSimulator
+{
+    public class Attack
+    {
+        public string Name { get; }
+        public ElementType Type { get; }
+        public int BasePower { get; }
+
+        public Attack(string name, ElementType type, int basePower)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Type = type;
+            BasePower = basePower;
+        }
+
+        public void Use(int level)
+        {
+            int totalPower = BasePower + level;
+            Console.WriteLine($"{Name} hits with total power {totalPower}.");
+        }
+    }
+}
--- PokemonTrainerSimulator/Program.cs
+++ PokemonTrainerSimulator/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace PokemonTrainerSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Attack flamethrower = new Attack("Flamethrower", ElementType.Fire, 12);
+            Attack inferno = new Attack("Inferno", ElementType.Fire, 6);
+            Attack waterGun = new Attack("Water Gun", ElementType.Water, 10);
+            Attack bubble = new Attack("Bubble", ElementType.Water, 5);
+            Attack thunder = new Attack("Thunder", ElementType.Electric, 15);
+            Attack spark = new Attack("Spark", ElementType.Electric, 8);
+        }
+    }
+}
+
+
