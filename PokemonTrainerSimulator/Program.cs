7237071cb8dfbf782b1493e6f9b36b3643937cd6
wpplng <lina.wappling@gmail.com>
2025-06-04 19:46:41 +0200

    Simulate the trainer

--- PokemonTrainerSimulator/Program.cs
+++ PokemonTrainerSimulator/Program.cs
@@ -6,12 +6,35 @@ namespace PokemonTrainerSimulator
     {
         static void Main(string[] args)
         {
+            // Initialize attacks for each ElementType
             Attack flamethrower = new Attack("Flamethrower", ElementType.Fire, 12);
             Attack inferno = new Attack("Inferno", ElementType.Fire, 6);
             Attack waterGun = new Attack("Water Gun", ElementType.Water, 10);
             Attack bubble = new Attack("Bubble", ElementType.Water, 5);
             Attack thunder = new Attack("Thunder", ElementType.Electric, 15);
             Attack spark = new Attack("Spark", ElementType.Electric, 8);
+
+            // Create instances of each Pokemon with their respective attacks
+            Charmander charmander = new Charmander(5, new List<Attack> { flamethrower, inferno });
+            Squirtle squirtle = new Squirtle(3, new List<Attack> { waterGun, bubble });
+            Pikachu pikachu = new Pikachu(4, new List<Attack> { thunder, spark });
+
+            // Create a list of Pokemons to simulate
+            List<Pokemon> pokemons = new List<Pokemon> { charmander, squirtle, pikachu };
+
+            // Run the simulation for each Pokemon
+            foreach (var pokemon in pokemons)
+            {
+                pokemon.RaiseLevel();
+                pokemon.Attack();
+
+                if (pokemon is IEvolvable evolvable)
+                {
+                    evolvable.Evolve();
+                }
+
+                Console.WriteLine();
+            }
         }
     }
 }
